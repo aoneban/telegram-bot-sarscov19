a6cce1cf8d2b8926a58812363768281d6ac22e3b
ashot <torgregion1@gmail.com>
2022-06-30 20:39:25 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const api = require('covid19-api');
+const M = require('telegraf-markup4');
+const COUNTRIES_LIST = require('./constant');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) =>
+  ctx.reply(
+    `
+Привет ${ctx.message.from.id}!
+Узнай статистику по коронавирусу.
+Введи название страны.
+Весь список стран можно узнать по команде /help
+`,
+M.keyboard.reply([
+      ['Belarus', 'Ukraine'],
+      ['Poland', 'Germany'],
+    ])
+  ));
+
+bot.help((ctx) => ctx.reply(COUNTRIES_LIST))
+bot.on('text', async (ctx) => {
+  let data = {};
+  try {
+    data = await api.getReportsByCountries(ctx.message.text);
+    const formatData = `
+Страна: ${data[0][0].country}
+Случаи: ${data[0][0].cases}
+Умерло: ${data[0][0].deaths}
+Выздоровело: ${data[0][0].recovered}
+  `;
+    ctx.reply(formatData);
+  } catch {
+    console.log('Ошибка');
+    ctx.reply('Ошибка, такой страны не существует! Посмотрите /help');
+  }
+});
+bot.launch();
+console.log('Бот уже запущен');
